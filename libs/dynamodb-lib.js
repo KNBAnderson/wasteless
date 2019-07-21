50d3caea42bfaf9f07f1d3f1e7514d00f5c1a463
Katlin <katlin.nbanderson@gmail.com>
2019-07-21 16:14:34 -0700

    refactor code to create api connection with DynomoDB and add logs for debugging 500 status errors

---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,8 @@
+import AWS from "aws-sdk";
+
+//Using the promise form of the DynamoDB methods. 
+export function call(action, params) {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return dynamoDb[action](params).promise();
+}
