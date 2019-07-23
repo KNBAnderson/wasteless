395342b1881471b21e4b285c74e5789776c99b75
Katlin Anderson <katlin@>
2019-07-23 16:26:43 -0700

    remove duplicate problems

--- libs/dynamodb-lib.js
+++
@@ -1,9 +0,0 @@
-import AWS from "aws-sdk";
-
-//Using the promise form of the DynamoDB methods.
-
-export function call(action, params) {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-  return dynamoDb[action](params).promise();
-}
