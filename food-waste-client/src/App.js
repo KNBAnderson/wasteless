ba10bb38533feed3561a497b9d1b908c5cb19b12
Katlin Anderson <katlin@>
2019-07-24 13:57:40 -0700

    add routing and route to basic home page

--- food-waste-client/src/App.js
+++ food-waste-client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import TopNav from "./containers/TopNav"
+import TopNav from "./containers/TopNav";
+import Routes from "./Routes";
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
       <TopNav />
+      <Routes />
       </div>
     );
   }
---
+++ food-waste-client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="Home">
+          <div className="lander">
+            <h1 className="logo">WASTE<span>LESS</span></h1>
+          </div>
+        </div>
+    );
+}
+
+export default Home;
