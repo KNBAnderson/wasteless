f65fbbdd45b88f5547717d6e229cb94f0d657a15
Katlin Anderson <katlin@>
2019-07-24 16:05:04 -0700

    add a Login page and route to it

--- food-waste-client/src/Routes.js
+++ food-waste-client/src/Routes.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Error404 from "./containers/Error404";
+import Login from "./containers/Login";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
     <Route component={Error404} />
   </Switch>;
