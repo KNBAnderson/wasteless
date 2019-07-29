93b28eaf63a476ee7b910387f7b7a920c256a6e5
Katlin Anderson <katlin@>
2019-07-29 10:16:22 -0700

    update get and refactor

--- food-waste-api/get.js
+++ food-waste-api/get.js
@@ -4,9 +4,6 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const params = {
     TableName: "ingredients",
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'ingredientId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       ingredientId: event.pathParameters.id
@@ -16,7 +13,6 @@ export async function main(event, context) {
   try {
     const result = await dynamoDbLib.call("get", params);
     if (result.Item) {
-      // Return the retrieved item
       return success(result.Item);
     } else {
       return failure({ status: false, error: "Item not found." });
