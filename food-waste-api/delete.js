a1577ab0e8027781aca00e3fa94f90b1bb09353a
Katlin Anderson <katlin@>
2019-07-29 10:36:28 -0700

    update delete and update functions

--- food-waste-api/delete.js
+++ food-waste-api/delete.js
@@ -4,9 +4,6 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const params = {
     TableName: "ingredients",
-    // 'Key' defines the partition key and sort key of the item to be removed
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'ingredientId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       ingredientId: event.pathParameters.id
--- food-waste-api/update.js
+++ food-waste-api/update.js
@@ -9,16 +9,10 @@ export async function main(event, context) {
       userId: event.requestContext.identity.cognitoIdentityId,
       ingredientId: event.pathParameters.id
     },
-    // 'UpdateExpression' defines the attributes to be updated
-    // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET content = :content, attachment = :attachment",
+    UpdateExpression: "SET ingredientName = :ingredientName",
     ExpressionAttributeValues: {
-      ":attachment": data.attachment || null,
-      ":content": data.content || null
+      ":ingredientName": data.ingredientName || null
     },
-    // 'ReturnValues' specifies if and how to return the item's attributes,
-    // where ALL_NEW returns all attributes of the item after the update; you
-    // can inspect 'result' below to see how it works with different settings
     ReturnValues: "ALL_NEW"
   };
 
