35ddd2b8cebe82ec98f27ae965ea7c3eb4237fb8
Katlin Anderson <example@example.com>
2019-07-31 12:14:46 -0700

    add and about section to the home page

--- food-waste-client/src/containers/Home.js
+++ food-waste-client/src/containers/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Home.css";
 import SplashBanner from './SplashBanner';
+import About from './About';
 
 function Home(props) {
     return (
         <div className="Home">
           <SplashBanner props={props}/>
+          <About/>
         </div>
     );
 }
--- food-waste-client/src/index.js
+++ food-waste-client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 import Amplify from "aws-amplify";
 import config from "./config";
+import './index.css';
 
 Amplify.configure({
   Auth: {
