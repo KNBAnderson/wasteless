61f876f06efa3e37f7dd6306d66aee1b3fdae9b2
Katlin <katlin.nbanderson@gmail.com>
2019-07-31 00:11:51 -0700

    make minor change

--- food-waste-client/src/containers/SplashBanner.js
+++ food-waste-client/src/containers/SplashBanner.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./SplashBanner.css";
 import { Button } from "react-bootstrap";
-import PropTypes from 'prop-types';
+import "./SplashBanner.css";
 
 function SplashBanner(props) {
 
     return (
         <div className="lander">
         <h1 className="logo">WASTE<span>LESS</span></h1>
-        {props.isAuthenticated ? <div id="banner-buttons">
+        {!props.props.isAuthenticated ? <div id="banner-buttons">
             <Link to="/signup">
                 <Button id="signup-button">Create an account</Button>
             </Link>
@@ -25,10 +24,5 @@ function SplashBanner(props) {
       </div>
     );
 }
-
-SplashBanner.propTypes = {
-  isAuthenticated: PropTypes.bool,
-  onLogout: PropTypes.func
-};
 
 export default SplashBanner;
