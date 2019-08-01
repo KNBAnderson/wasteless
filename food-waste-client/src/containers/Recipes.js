a895012721e066fc156558b466e10a5729ca30d5
Katlin Anderson <example@example.com>
2019-07-31 21:02:55 -0700

    styles recipes page

--- food-waste-client/src/containers/Recipes.js
+++ food-waste-client/src/containers/Recipes.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 import "./Recipes.css";
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
+function findUsedIngredients(recipe) {
+  let array = [];
+  recipe.usedIngredients.forEach(element => {
+    array.push(element.name);
+  });
+  return array.join(", ");
+}
+
+function findMissedIngredients(recipe) {
+  let array = [];
+  recipe.missedIngredients.forEach(element => {
+    array.push(element.name);
+  });
+  return array.join(", ");
+}
 
 export default class Recipes extends Component {
   constructor(props) {
     super(props);
   }
+
 
   render() {
     console.log(this.props.recipes.length);
@@ -13,17 +31,24 @@ export default class Recipes extends Component {
     return (
       <div className="recipes">
         <h1>Recipes</h1>
-        {this.props.recipes.map(
-          recipe =>
-          this.props.recipes.length !==0
-          ? <li 
-            key = {recipe.id}>
-            {recipe.title}
-          </li>
-          : <h4>
-              You have no recipes in your pantry yet
-            </h4>
-        )}
+        <div className="recipes-container">
+          {this.props.recipes.map(
+            recipe =>
+            this.props.recipes.length !==0
+            ? <Card>
+            <CardImg top width="100%" src={recipe.image} alt="Card image cap" />
+            <CardBody>
+              <h3>{recipe.title}</h3>
+              <CardSubtitle>You have {findUsedIngredients(recipe)}</CardSubtitle>
+              <hr/>
+              <CardText>You will need {findMissedIngredients(recipe)}</CardText>
+            </CardBody>
+            </Card>
+            : <h4>
+                You have no recipes in your pantry yet
+              </h4>
+          )}
+        </div>
       </div>
     );
   }
