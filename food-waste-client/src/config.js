7c9965e6fa7fddd8b2a6d31f2568d032b31832ac
KNBAnderson <46687472+KNBAnderson@users.noreply.github.com>
2020-12-02 21:40:33 -0800

    Create config.js
    I know there must be a better way...

---
+++ food-waste-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "",
+      BUCKET: ""
+    },
+    apiGateway: {
+      REGION: "",
+      URL: ""
+    },
+    cognito: {
+      REGION: "",
+      USER_POOL_ID: "",
+      APP_CLIENT_ID: "",
+      IDENTITY_POOL_ID: ""
+    }
+  };
